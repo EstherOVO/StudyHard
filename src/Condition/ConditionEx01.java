2ce55b7808394f601b353c19a97a2a923cc35a50
EstherOVO <tmejwjd@gmail.com>
2024-02-09 15:13:27 +0900

    Java Condition_Examples

--- src/Condition/ConditionEx01.java
+++ src/Condition/ConditionEx01.java
@@ -11,21 +11,35 @@ public class ConditionEx01 {
         달러를 0원 입금 받으면 "환전할 금액이 없습니다."
         양수일 때 환전금액은 "00000원 입니다."
         음수일 때 "잘못 입력하셨습니다"
- */
+*/
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("환전하실 금액(달러)을 입력해 주세요 : ");
-        int dollar = scanner.nextInt();
+        System.out.print("환전하실 금액(달러)를 입력해 주세요 : ");
+        String enteredDollar = scanner.nextLine();
 
-        int won = dollar * 1327;
+        System.out.print("총 환전하실 금액이 " + enteredDollar + "달러가 맞습니까? (Y / N) : ");
+        String enteredAnswer = scanner.nextLine();
 
-        if (dollar == 0) {
-            System.out.println("환전할 금액이 없습니다.");
-        } else if (dollar > 0) {
-            System.out.println("총 환전 금액은 " + won + "원입니다.");
-        } else if (dollar < 0) {
-            System.out.println("잘못 입력했습니다. 다시 시도해 주세요.");
+        scanner.close();
+
+        int enteredDollar2 = Integer.parseInt(enteredDollar);
+        int exchangeWon = enteredDollar2 * 1327;
+
+        switch (enteredAnswer) {
+            case "Y" :
+                if (enteredDollar2 == 0) {
+                    System.out.println("환전하실 금액(달러)가 없습니다.");
+                } else if (enteredDollar2 > 0) {
+                    System.out.println("총 환전 금액은 " + exchangeWon + "원입니다.");
+                } else if (enteredDollar2 < 0) {
+                    System.out.println("잘못 입력하셨습니다. 금액(달러)을 확인해 주시기를 바랍니다.");
+                }
+                break;
+
+            case "N" :
+                System.out.println("'N'를 입력하셨습니다.\n처음부터 다시 시도해 주시기를 바랍니다.");
+                break;
         }
 
     }

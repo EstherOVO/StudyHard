712dad1c2f3d7c28401a9aa73995931f87ea00b5
EstherOVO <tmejwjd@gmail.com>
2024-02-28 18:58:48 +0900

    Java Nested Class

---
+++ src/interface00/Franchise.java
@@ -0,0 +1,45 @@
+package interface00;
+
+public class Franchise {
+
+    String storeName;
+
+    public Franchise(String storeName) {
+        this.storeName = storeName;
+    }
+
+    static class TaskForce {
+
+        String branchName;
+        String leader;
+
+        public TaskForce(String branchName, String leader) {
+            this.branchName = branchName;
+            this.leader = leader;
+            System.out.println("리더 " + leader + "님을 선두로 " + branchName + " TF 팀 출두!");
+        }
+
+        public Franchise launch() {
+            System.out.println(branchName + " 오픈!");
+            return new Franchise(branchName);
+        }
+    }
+
+    class Coupon {
+
+        int amount;
+        String to;
+        String message;
+
+        public Coupon(int amount, String to) {
+            this.amount = amount;
+            this.to = to;
+            this.message = "%s에서 %d원 쿠폰을 %s님께 드립니다.".formatted(storeName, amount, to);
+            System.out.println(message);
+        }
+    }
+
+    public Coupon getCoupon(int amount, String to) {
+        return new Coupon(amount, to);
+    }
+}
---
+++ src/interface00/FranchiseMain.java
@@ -0,0 +1,17 @@
+package interface00;
+
+import interface00.Franchise;
+
+public class FranchiseMain {
+    public static void main(String[] args) {
+
+        Franchise.TaskForce taskForce1 = new Franchise.TaskForce("부산점", "홍길동");
+        Franchise busanBranch = taskForce1.launch();
+
+        Franchise.TaskForce taskForce2 = new Franchise.TaskForce("서울점", "김자바");
+        Franchise seoulBranch = taskForce2.launch();
+
+        busanBranch.getCoupon(10000, "홍길동");
+        seoulBranch.getCoupon(5000, "김자바");
+    }
+}

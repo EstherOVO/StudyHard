8fbba3dd39fca11e6e40daf73baa60b7a6275405
EstherOVO <tmejwjd@gmail.com>
2024-03-06 21:01:18 +0900

    Java String Exercises

--- src/library/Ex02.java
+++ src/library/Ex02.java
@@ -21,18 +21,18 @@ public class Ex02 {
 
 //      1.
         String[] totalNumber = people.split(";");
-        System.out.println("1. 전체 사용자 수 : " + totalNumber.length + "명");
+        System.out.println("▣ 전체 사용자 수 : " + totalNumber.length + "명");
 
 //      2.
-        System.out.println("2. 사용자 정보 : ");
+        System.out.println("▣ 사용자 정보 : ");
         String[] splitSemiColon = people.split(";");
         for (int i = 0; i < splitSemiColon.length; i++) {
             String[] splitComma = splitSemiColon[i].split(",");
-            System.out.println("■ 이름 : " + splitComma[0] + " | ■ 나이 : " + splitComma[1] + " | ■ 이메일 : " + splitComma[2]);
+            System.out.println("(" + (i + 1) + ") 이름 : " + splitComma[0] + " | 나이 : " + splitComma[1] + "세 | 이메일 : " + splitComma[2]);
         }
 
-//      4.
-        System.out.print("4. \"gmail.com\" 도메인 사용자 : ");
+//      3.
+        System.out.print("▣ \"gmail.com\" 도메인 사용자 : ");
         for (int j = 0; j < splitSemiColon.length; j++) {
             if (splitSemiColon[j].contains("gmail.com")) {
                 if (j == splitSemiColon.length - 1) {
@@ -44,8 +44,8 @@ public class Ex02 {
         }
         System.out.println();
 
-//      5.
-        System.out.println("5. 가장 나이가 많은 사용자 : ");
+//      4.
+        System.out.println("▣ 가장 나이가 많은 사용자 : ");
         int[] ages = new int[4];
         for (int i = 0; i < splitSemiColon.length; i++) {
             String[] splitComma = splitSemiColon[i].split(",");
@@ -62,12 +62,12 @@ public class Ex02 {
 
         for (int i = 0; i < splitSemiColon.length; i++) {
             if (splitSemiColon[i].contains(strMax)) {
-                System.out.println("■ 이름 : " + splitSemiColon[i].substring(0, 3) + " | ■ 나이 : " + max + "세");
+                System.out.println("이름 : " + splitSemiColon[i].substring(0, 3) + " | 나이 : " + max + "세");
             }
         }
 
-//      6.
+//      5.
         int kimCount = people.length() - people.replace("김", "").length();
-        System.out.println("6. 이름에 \"김\"이 포함된 사용자 수 : " + kimCount + "명");
+        System.out.println("▣ 김가네 : " + kimCount + "명");
     }
 }

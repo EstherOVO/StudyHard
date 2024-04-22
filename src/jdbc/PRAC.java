82897eefcd8ab8a9ecb39c0fd20cb3f0650f63d7
EstherOVO <tmejwjd@gmail.com>
2024-04-22 20:33:34 +0900

    Java JDBC(Java Database Connectivity) Exercises

--- src/jdbc/PRAC.java
+++ src/jdbc/PRAC.java
@@ -6,26 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class Ex02 {
+public class PRAC {
     public static void main(String[] args) {
-
-/*
-        연습 문제 2. 사용자 정보 업데이트
-
-        요구사항 :
-        사용자로부터 userId, name, password, age, email을 입력받아
-        해당 userId를 가진 사용자의 정보를 업데이트하세요.
-
-        사용자로부터 다음 정보를 입력받습니다.
-
-        userId : 업데이트 할 사용자 ID
-        name : 새 사용자 이름
-        password : 새 비밀번호
-        age : 새 나이
-        email : 새 이메일 주소
-
-        - 입력받은 정보를 데이터베이스의 users 테이블에 업데이트합니다.
-*/
 
         String url = "jdbc:mysql://localhost:3306/jdbc";
         String user = "root";
@@ -48,51 +30,50 @@ public class Ex02 {
         System.out.print("§ 이메일을 입력하세요 : ");
         String email = scanner.nextLine();
 
-/////////////////////////////////////////////////////////////
+        ////////////////////////////////////////
 
-        StringBuilder updateSql = new StringBuilder();
-
-        updateSql.append("UPDATE users SET");
+        StringBuilder sb = new StringBuilder();
+        sb.append("UPDATE users SET");
         boolean isFirst = true;
 
         if (!name.isEmpty()) {
             if (!isFirst) {
-                updateSql.append(",");
+                sb.append(",");
             }
-            updateSql.append(" name = ?");
+            sb.append(" name = ?");
             isFirst = false;
         }
 
         if (!pw.isEmpty()) {
             if (!isFirst) {
-                updateSql.append(",");
+                sb.append(",");
             }
-            updateSql.append(" password = ?");
+            sb.append(" password = ?");
             isFirst = false;
         }
 
         if (!age.isEmpty()) {
             if (!isFirst) {
-                updateSql.append(",");
+                sb.append(",");
             }
-            updateSql.append(" age = ?");
+            sb.append(" age = ?");
             isFirst = false;
         }
 
         if (!email.isEmpty()) {
             if (!isFirst) {
-                updateSql.append(",");
+                sb.append(",");
             }
-            updateSql.append(" email = ?");
+            sb.append(" email = ?");
             isFirst = false;
         }
 
-        updateSql.append(" WHERE userID = ?");
+        sb.append(" WHERE userID = ?");
 
         int index = 1;
 
         try (Connection conn = DriverManager.getConnection(url, user, password);
-             PreparedStatement pstmt = conn.prepareStatement(updateSql.toString())) {
+             PreparedStatement pstmt = conn.prepareStatement(sb.toString())) {
 
             if (!name.isEmpty()) pstmt.setString(index++, name);
             if (!pw.isEmpty()) pstmt.setString(index++, pw);
@@ -103,10 +84,10 @@ public class Ex02 {
 
             int rows = pstmt.executeUpdate();
 
-            System.out.println("사용자 <" + id + ">님의 정보가 정상적으로 업데이트 되었습니다.");
+            System.out.println("업데이트 완료!");
 
         } catch (SQLException e) {
-            System.out.println("다시 시도해 주시기를 바랍니다.");
+            throw new RuntimeException(e);
         }
     }
 }
